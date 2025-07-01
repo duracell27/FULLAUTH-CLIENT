7c7ec2683f4235acabc24ee2cb81607dde53bd2f
Duracell27 <volodymyrshmidt@gmail.com>
2025-07-01 17:17:15 +0300

    fix bg

--- src/app/expenses/[expenseId]/ExpenseData.tsx
+++ src/app/expenses/[expenseId]/ExpenseData.tsx
@@ -13,10 +13,11 @@ import {
 	DialogTitle,
 	DialogTrigger
 } from '@/shared/componets/ui'
+import { BackButton } from '@/shared/componets/ui/BackButton'
 import { useExpense } from '@/shared/hooks/useExpense'
 import { formatBalance } from '@/shared/utils/formatBalance'
 import { format } from 'date-fns'
-import { Calendar, ContactRound, Eye } from 'lucide-react'
+import { Calendar, ContactRound, Eye, HandCoins, MoveRight } from 'lucide-react'
 import Image from 'next/image'
 import React from 'react'
 
@@ -38,42 +39,64 @@ const ExpenseData = ({ expenseId }: Props) => {
 	}
 	return (
 		<div className='w-full max-w-[400px] flex flex-col gap-3 pb-18'>
+			<BackButton />
 			<Card className=''>
 				<CardHeader className='p-0'>
 					<CardTitle
-						className='flex relative gap-2 items-center justify-between h-50 bg-cover bg-center rounded-lg px-5'
-						style={{ backgroundImage: `url(${expense.photoUrl})` }}
+						className={`flex relative gap-2 items-center justify-between h-50 rounded-lg px-5 ${
+							expense.photoUrl === ''
+								? 'bg-primary/40'
+								: 'bg-cover bg-center'
+						}`}
+						style={
+							expense.photoUrl !== ''
+								? {
+										backgroundImage: `url(${expense.photoUrl})`
+								  }
+								: {}
+						}
 					>
+						{expense.photoUrl === '' && (
+							<div className='w-full flex items-center justify-center  text-background '>
+								<span className='bg-primary rounded-full px-5 py-2'>
+									{expense.description
+										.slice(0, 2)
+										.toUpperCase()}
+								</span>
+							</div>
+						)}
 						<div className='absolute -bottom-2 inset-x-5 gap-5 flex justify-between items-center  text-background bg-primary rounded-full px-5 py-2'>
 							<h1 className=''>{expense.description}</h1>
 							<span>{formatBalance(expense.amount)}</span>
 						</div>
 
-						<div className='absolute top-3 right-3'>
-							<Dialog>
-								<DialogTrigger>
-									<DialogTitle></DialogTitle>
-									<div className='rounded-full bg-white/30 text-background/50 p-1'>
-										<Eye />
-									</div>
-								</DialogTrigger>
-								<DialogContent className='w-[90vw] max-w-none'>
-									<Image
-										src={expense.photoUrl || ''}
-										alt=''
-										width={0}
-										height={0}
-										sizes='100vw'
-										style={{
-											width: '100%',
+						{expense.photoUrl !== '' && (
+							<div className='absolute top-3 right-3'>
+								<Dialog>
+									<DialogTrigger>
+										<DialogTitle></DialogTitle>
+										<div className='rounded-full bg-white/30 text-background/50 p-1'>
+											<Eye />
+										</div>
+									</DialogTrigger>
+									<DialogContent className='w-[90vw] max-w-none'>
+										<Image
+											src={expense.photoUrl || ''}
+											alt=''
+											width={0}
+											height={0}
+											sizes='100vw'
+											style={{
+												width: '100%',
 
-											height: 'auto',
-											borderRadius: '8px'
-										}}
-									/>
-								</DialogContent>
-							</Dialog>
-						</div>
+												height: 'auto',
+												borderRadius: '8px'
+											}}
+										/>
+									</DialogContent>
+								</Dialog>
+							</div>
+						)}
 					</CardTitle>
 				</CardHeader>
 				<CardContent>
@@ -121,8 +144,11 @@ const ExpenseData = ({ expenseId }: Props) => {
 				<CardContent>
 					<ul className='space-y-3'>
 						{expense.payers.map(payer => (
-							<li key={payer.id} className='flex items-center gap-2'>
-								<div className="">
+							<li
+								key={payer.id}
+								className='flex items-center gap-2'
+							>
+								<div className=''>
 									<Avatar>
 										<AvatarImage
 											src={
@@ -143,16 +169,29 @@ const ExpenseData = ({ expenseId }: Props) => {
 								</div>
 								<div className='w-full'>
 									<p>{payer.payer.displayName}</p>
-									<div className="h-3 relative flex items-center bg-good-green/20 rounded-3xl pl-1">
-										<div className="h-1 bg-good-green rounded-l-full" style={{width: `${(payer.amount*100)/expense.amount}%`}}></div>
-										<div className="bg-good-green px-1 text-background rounded-full text-xs">{payer.amount}</div>
+									<div className='h-3 relative flex items-center bg-good-green/20 rounded-3xl pl-1'>
+										<div
+											className='h-1 bg-good-green rounded-l-full'
+											style={{
+												width: `${
+													(payer.amount * 100) /
+													expense.amount
+												}%`
+											}}
+										></div>
+										<div className='bg-good-green px-1 text-background rounded-full text-xs'>
+											{formatBalance(payer.amount)}
+										</div>
 									</div>
 								</div>
 							</li>
 						))}
 						{expense.splits.map(splitter => (
-							<li key={splitter.id} className='flex items-center gap-2'>
-								<div className="">
+							<li
+								key={splitter.id}
+								className='flex items-center gap-2'
+							>
+								<div className=''>
 									<Avatar>
 										<AvatarImage
 											src={
@@ -172,10 +211,29 @@ const ExpenseData = ({ expenseId }: Props) => {
 									</Avatar>
 								</div>
 								<div className='w-full'>
-									<p>{splitter.debtor.displayName}</p>
-									<div className="h-3 relative flex items-center bg-bad-red/20 rounded-3xl pl-1" style={{width: `${(splitter.amount*100)/expense.amount}%`}}>
-										<div className="h-1 bg-bad-red rounded-l-full w-full"></div>
-										<div className="bg-bad-red px-1 text-background rounded-full text-xs">{splitter.amount}</div>
+									<div className='flex items-center gap-2'>
+										<p>{splitter.debtor.displayName}</p>
+										<div className='text-xs text-neutral-grey flex items-center gap-1'>
+											<MoveRight className='size-4' />
+											<HandCoins className='size-4' />
+											<p>
+												{splitter.creditor.displayName}
+											</p>
+										</div>
+									</div>
+									<div
+										className='h-3 relative flex items-center bg-bad-red/20 rounded-3xl pl-1'
+										style={{
+											width: `${
+												(splitter.amount * 100) /
+												expense.amount
+											}%`
+										}}
+									>
+										<div className='h-1 bg-bad-red rounded-l-full w-full'></div>
+										<div className='bg-bad-red px-1 text-background rounded-full text-xs'>
+											{formatBalance(splitter.amount)}
+										</div>
 									</div>
 								</div>
 							</li>
@@ -186,7 +244,9 @@ const ExpenseData = ({ expenseId }: Props) => {
 			<Card>
 				<CardContent className='flex justify-center items-center gap-3 pt-3'>
 					<Button className='px-5'>Edit</Button>
-					<Button className='px-5 ' variant={'danger'}>Delete</Button>
+					<Button className='px-5 ' variant={'danger'}>
+						Delete
+					</Button>
 				</CardContent>
 			</Card>
 		</div>
