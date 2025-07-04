c17323c9fbb5adc67fe6f673a403eaad6e884c88
Duracell27 <volodymyrshmidt@gmail.com>
2025-07-04 15:31:03 +0300

    fix session 1

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,36 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  env: {
-    SERVER_URL: process.env.SERVER_URL,
-    GOOGLE_RECAPTCHA_SITE_KEY: process.env.GOOGLE_RECAPTCHA_SITE_KEY,
-    FRONTEND_URL: process.env.NEXT_PUBLIC_FRONTEND_URL,
-    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
-    CLOUDINARY_UPLOAD_PRESET: process.env.CLOUDINARY_UPLOAD_PRESET,
-    GOOGLE_ANALYTICS: process.env.GOOGLE_ANALYTICS,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-      },
-    ]
-  }
-};
+	env: {
+		SERVER_URL: process.env.SERVER_URL,
+		SERVER_PATH: process.env.SERVER_PATH,
+		DEV_MODE: process.env.DEV_MODE,
+		GOOGLE_RECAPTCHA_SITE_KEY: process.env.GOOGLE_RECAPTCHA_SITE_KEY,
+		FRONTEND_URL: process.env.NEXT_PUBLIC_FRONTEND_URL,
+		CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+		CLOUDINARY_UPLOAD_PRESET: process.env.CLOUDINARY_UPLOAD_PRESET,
+		GOOGLE_ANALYTICS: process.env.GOOGLE_ANALYTICS
+	},
+	async rewrites() {
+		return [
+			{
+				source: `${process.env.SERVER_PATH}/:path*`,
+				destination: `${process.env.SERVER_URL}/:path*`
+			}
+		]
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'lh3.googleusercontent.com'
+			},
+			{
+				protocol: 'https',
+				hostname: 'res.cloudinary.com'
+			}
+		]
+	}
+}
 
-export default nextConfig;
+export default nextConfig
--- src/shared/utils/api/instance.api.ts
+++ src/shared/utils/api/instance.api.ts
@@ -1,8 +1,11 @@
-import { FetchClient } from "../fetch/fetchClient";
+import { FetchClient } from '../fetch/fetchClient'
 
 export const api = new FetchClient({
-    baseUrl: process.env.SERVER_URL as string,
-    options:{
-        credentials: 'include'
-    }
+	baseUrl:
+		process.env.DEV === 'DEV'
+			? (process.env.SERVER_URL as string)
+			: (process.env.SERVER_PATH as string),
+	options: {
+		credentials: 'include'
+	}
 })
